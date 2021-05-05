eaac3dbfe6ea626ec2df26dcda021280dbb5aaa9
Rowan Trollope <rowantrollope@Rowans-MacBook-Air.local>
2021-05-04 23:35:17 -0700

    changes

--- src/components/MyNav.js
+++ src/components/MyNav.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Identicon from 'react-identicons';
+//import Identicon from 'react-identicons';
 import dbank from '../dbank.png';
 
 class MyNav extends Component {
@@ -16,17 +16,6 @@ class MyNav extends Component {
                 <Navbar.Collapse className="justify-content-end">
                     <Navbar.Text>
                         Account number: <a href="#login">{this.props.account}  </a>
-                        <Identicon string='{this.props.account}' size="20" />
-                        { /*this.props.account
-                            ? <img
-                                className="ml-2"
-                                width='30'
-                                height='30'
-                                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
-                                alt=""
-                            />
-                            : <span></span>
-                        */} 
                     </Navbar.Text>
                 </Navbar.Collapse>
             </Navbar>
@@ -35,3 +24,5 @@ class MyNav extends Component {
 }
 
 export default MyNav;
+
+//<Identicon string='{this.props.account}' size="20" />
